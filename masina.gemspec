eab04c6025f8ee8974967d9cbd2bbca533ffb8fa
Aleksandar Popovic <aleksandar.popovic.popac@gmail.com>
2019-10-15 00:53:40 +0200

    Set coverage

--- masina.gemspec
+++ masina.gemspec
@@ -31,5 +31,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'rake', '~> 10.0'
   spec.add_development_dependency 'rspec', '~> 3.0'
   spec.add_development_dependency 'rubocop', '~> 0.74.0'
+  spec.add_development_dependency 'coveralls'
   spec.add_runtime_dependency 'rails', '~> 6.0'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,11 @@
-require "bundler/setup"
-require "masina"
+require 'bundler/setup'
+require 'masina'
+require 'coveralls'
+Coveralls.wear!
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
