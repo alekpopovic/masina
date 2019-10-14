c1678532f8bb9b6bc9b2533a290c353ae8ecb12b
Aleksandar Popovic <aleksandar.popovic.popac@gmail.com>
2019-10-14 20:37:42 +0200

    Update policy_generator.rb

--- lib/generators/policy/policy_generator.rb
+++ lib/generators/policy/policy_generator.rb
@@ -1,19 +1,25 @@
+require 'rails/generators'
+
 module Masina
-  class PolicyGenerator < Rails::Generators::NamedBase
-    source_root File.expand_path('templates', __dir__)
-    argument :methods, type: :array, default: [], banner: 'method method'
+  module Generators
+    module Policy
+      class PolicyGenerator < Rails::Generators::NamedBase
+        source_root File.expand_path('templates', __dir__)
+        argument :methods, type: :array, default: [], banner: 'method method'
 
-    def create_policy_file
-      policy_dir_path = 'app/policies'
-      generator_path = policy_dir_path + "/#{file_name}_policy.rb"
-      Dir.mkdir(policy_dir_path) unless File.exist?(policy_dir_path)
-      template 'policy.erb', generator_path
-    end
+        def create_policy_file
+          policy_dir_path = 'app/policies'
+          generator_path = policy_dir_path + "/#{file_name}_policy.rb"
+          Dir.mkdir(policy_dir_path) unless File.exist?(policy_dir_path)
+          template 'policy.erb', generator_path
+        end
 
-    private
+        private
 
-    def methods?
-      methods.any?
+        def methods?
+          methods.any?
+        end
+      end
     end
   end
 end
